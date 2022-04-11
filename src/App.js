d58a022a8fd7ed6c74fa8de54dc2e07d64a3374c
D.I.P Lakshan <onlineipl18@gmail.com>
2022-04-11 21:28:15 +0530

    make changes

--- src/App.js
+++ src/App.js
@@ -5,13 +5,25 @@ function App() {
       id: 1,
       titel: "Toilet Papers",
       amount: 245.6,
-      date: new Date(2022, 3, 20),
+      date: new Date(2022, 1, 20),
     },
     {
       id: 2,
       titel: "Insuarnce",
       amount: 255.69,
-      date: new Date(2022, 3, 25),
+      date: new Date(2022, 3, 14),
+    },
+    {
+      id: 3,
+      titel: "Papers",
+      amount: 55.69,
+      date: new Date(2022, 4, 25),
+    },
+    {
+      id: 4,
+      titel: "Helth",
+      amount: 855.69,
+      date: new Date(2022, 5, 25),
     },
   ];
   return (
@@ -26,6 +38,16 @@ function App() {
         titel={expenses[1].titel}
         amount={expenses[1].amount}
         date={expenses[1].date}
+      />
+      <ExpenseItem
+        titel={expenses[2].titel}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      />
+      <ExpenseItem
+        titel={expenses[3].titel}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
       />
     </div>
   );
