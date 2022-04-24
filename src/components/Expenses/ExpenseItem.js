8fd0de66898c424948e124569144da732eb053ef
D.I.P Lakshan <onlineipl18@gmail.com>
2022-04-24 08:07:50 +0530

    woriking with state

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,26 @@
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+  const [titel, setTitel] = useState(props.titel);
+
+  const titelChangeHandeler = () => {
+    setTitel("Updated");
+    console.log(titel);
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.titel}</h2>
+        <h2>{titel}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button type="submit" onClick={titelChangeHandeler}>
+        Change Titel
+      </button>
     </Card>
   );
 }
